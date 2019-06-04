29b43040edd3ee191cf2fa5d7f0c70f0d7262273
jrmccann2 <jshmccann@gmail.com>
2019-06-04 11:45:59 -0600

    added boilerplate and basic component tree

---
+++ src/components/Calendar/Calendar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+// import './Home.css';
+
+function Calendar (props) {
+    return (
+      <div style={{color: 'purple'}}>{props.num}</div>
+    )
+}
+
+export default Calendar;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+import Calendar from '../Calendar/Calendar'
+import './Home.css';
+
+class Home extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      dumbData: [
+        {num: "1"},
+        {num: "2"},
+        {num: "3"},
+        {num: "4"},
+        {num: "5"}
+      ],
+      showAmt: 3
+    }
+  }
+
+
+  render() {
+    const arrToMap = [...this.state.dumbData]
+    const mappedNums = arrToMap
+          .slice(0, this.state.showAmt)
+          .map((numObj, i) => {
+            return (
+              <Calendar key={i} num={numObj.num}/>
+            )
+          })
+    return (
+      <div className="App">
+        <header className="App-header">
+          Home
+        </header>
+        {mappedNums}
+      </div>
+    )
+  }
+}
+
+export default Home;
