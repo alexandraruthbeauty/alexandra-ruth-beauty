64ffff458a4b02b92bdb9e3b26abb0bc3f883961
jrmccann2 <jshmccann@gmail.com>
2019-09-23 11:52:19 -0600

    built out basic components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,42 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Calendar from '../Calendar/Calendar'
-import './Home.css';
+import Calendar from "../Calendar/Calendar";
+import "./Home.css";
 
 class Home extends Component {
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
       dumbData: [
-        {num: "1"},
-        {num: "2"},
-        {num: "3"},
-        {num: "4"},
-        {num: "5"}
+        { num: "1" },
+        { num: "2" },
+        { num: "3" },
+        { num: "4" },
+        { num: "5" }
       ],
       showAmt: 3
-    }
+    };
   }
 
-
   render() {
-    const arrToMap = [...this.state.dumbData]
+    const arrToMap = [...this.state.dumbData];
     const mappedNums = arrToMap
-          .slice(0, this.state.showAmt)
-          .map((numObj, i) => {
-            return (
-              <Calendar key={i} num={numObj.num}/>
-            )
-          })
+      .slice(0, this.state.showAmt)
+      .map((numObj, i) => {
+        return <Calendar key={i} num={numObj.num} />;
+      });
     return (
       <div className="App">
         <header className="App-header">
           Home
+          {mappedNums}
         </header>
-        {mappedNums}
       </div>
-    )
+    );
   }
 }
 
